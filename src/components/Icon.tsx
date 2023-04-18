498cb3db1f6c99655fd12546787421fa5b92609f
Onur Ravli <onurravli@hotmail.com>
2023-04-19 02:37:14 +0300

    fix: remove unnec. icon file

--- src/components/Icon.tsx
+++
@@ -1,7 +0,0 @@
-import icon from "/public/ravlico-black";
-import styles from "@/styles/Icon.module.css";
-import Image from "next/image";
-
-export default function Icon({ width, height }: { width: number; height: number }) {
-  return <Image className={styles.icon} width={width} height={height} src={icon} alt={"Ravlico Icon"} />;
-}
